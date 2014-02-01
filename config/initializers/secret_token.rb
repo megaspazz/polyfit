5d2b951b50e9a8fd8e6ba215767d998d536d8103
Erik Pejka <epejka@localhost.localdomain>
2014-02-01 00:52:48 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polyfit::Application.config.secret_key_base = '4ba2d799f190def677b19d0e50c6b8385a20d5cb40451aecf64a8ee4d7b2cb21afc1a32794c04c10a524b724adf642a27332c1ca3d6a9106aa6f8062858425e1'
