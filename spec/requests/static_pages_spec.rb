31e799b43805bc2bfdf86d73f19dea9e47f34486
Jonathan Hwang <jhwang05@gmail.com>
2014-02-04 22:54:52 -0500

    Set up rspec testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,16 @@ require 'spec_helper'
 describe "Static pages" do
 
 	describe "Test page" do
+
+		#it "should have the title 'TEST PAGE" do
+		#	visit '/test'
+		#	expect(page).to have_title('TEST PAGE')
+		#end
 		
 		it "should have the content 'Test page'" do
 			visit '/test'
 			expect(page).to have_content('TEST PAGE')
     end
   end
+
 end
