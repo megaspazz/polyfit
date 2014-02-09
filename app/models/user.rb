6cf3c9256ca74a7e2b2ff1e72c2721f687a3197f
Jonathan Hwang <jhwang05@gmail.com>
2014-02-08 23:33:03 -0500

    Sign in using either username or password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,27 @@ class User < ActiveRecord::Base
   #validates :firstName, presence: true
   #validates :lastName, presence: true
   
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 
   # Use a regex to validate the email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
   # bcryp authentication, minimum password length is 5
   #has_secure_password
   #validates :password, length: { minimum: 5 }
+
+  #edit database authentication
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+      else
+        where(conditions).first
+      end
+    end
 end
